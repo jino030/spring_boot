55bf8ec7a4978341d13a89e1d83d0b0fa24f1ecd
전유진(Jeon Yu Jin) <sok030614@gmail.com>
2023-11-15 17:56:03 +0900

    20231115 스프링부트

---
+++ sp02/src/main/java/com/yedam/app/TestController.java
@@ -0,0 +1,21 @@
+package com.yedam.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/")
+	public String viewTest(Model model) {
+		model.addAttribute("info", "타임리프가 적용된 경우");
+		return "viewTest";
+	}
+	
+	@GetMapping("/selectInfo")
+	public String viewSelectInfo(Model model) {
+		model.addAttribute("info", "링크 테스트합니다.");
+		return "selectInfo";
+	}
+}
---
+++ sp02/src/main/java/com/yedam/app/WebConfig.java
@@ -0,0 +1,22 @@
+package com.yedam.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Value("${file.loading.path}")
+	private String loadingPath;
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		System.out.println(loadingPath);
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations(loadingPath)
+                .setCachePeriod(100);
+    }
+
+}
